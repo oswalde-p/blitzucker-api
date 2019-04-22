526ff6467310d9eb628db1cce9a5a93e2484548b
Jason Pursey <jason@unumotors.com>
2019-04-22 22:28:10 +0200

    remove dotenv

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,4 @@
-require('dotenv').config()
-
 const config = {}
 config.PORT = process.env.PORT || 3001
-config.MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || 'No mongo string found'
+config.MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/d6s'
 module.exports = config
